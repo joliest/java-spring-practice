7a3fd79e41cce061a6758693543f1c11bec295fe
Joliver Estampador <joliverestampador@gmail.com>
2019-11-16 11:47:54 +0800

    Using @RequestParams in MVC

--- spring workspace/spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ spring workspace/spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -28,6 +29,20 @@ public class HelloWorldController {
 		theName = theName.toUpperCase();
 		
 		String result = "Yo! " + theName;
+		
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
+	
+	@RequestMapping("/processFormVersionThree")
+	public String letsShoutDudeAlternative(
+			@RequestParam("studentName") String theName, 
+			Model model) {
+		
+		theName = theName.toUpperCase();
+		
+		String result = "Hey my friend from v3! " + theName;
 		
 		model.addAttribute("message", result);
 		

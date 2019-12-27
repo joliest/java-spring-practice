210337c98ba1c8f72d3f2e6ff59fa031eb75497e
Joliver Estampador <joliverestampador@gmail.com>
2019-12-27 10:39:51 +0800

    First AOP @Before

---
+++ spring workspace/spring-demo-AOP/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,18 @@
+package com.luv2code.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyDemoLoggingAspect {
+	// add all related advices for logging
+	
+	// start wth @Before advise
+	
+	@Before("execution(public void addAccount())")
+	public void beforeAddAccountAdvice() {
+		System.out.println("\n====>>> Executing @Before advise on addAccount");
+	}
+}

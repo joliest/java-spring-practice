e4fee0aec42cce9835ba3dfc46cca3e9fd9a82d4
Joliver Estampador <joliverestampador@gmail.com>
2020-02-09 14:27:13 +0800

    Restring users accessig sites by role

---
+++ spring workspace 2/spring-security-demo-09-restrict-access-by-roles/src/main/java/com/luv2code/springsecurity/demo/controller/LoginController.java
@@ -0,0 +1,33 @@
+package com.luv2code.springsecurity.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+	
+	@GetMapping("/showMyLoginPage")
+	public String showMyLoginPage() {
+		return "fancy-login";
+	}
+	
+	@GetMapping("/")
+	public String showLanding() {
+		return "landing";
+	}
+	
+	@GetMapping("/employees")
+	public String showHome() {
+		return "home";
+	}
+	
+	@GetMapping("/leaders")
+	public String showLeaders() {
+		return "leaders";
+	}
+	
+	@GetMapping("/systems")
+	public String showSystems() {
+		return "systems";
+	}
+}

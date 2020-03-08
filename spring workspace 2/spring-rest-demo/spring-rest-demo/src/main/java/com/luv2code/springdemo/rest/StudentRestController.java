483d2864a55a7e058af9076d1e9faf701194e389
Joliver Estampador <joliverestampador@gmail.com>
2020-03-08 11:05:05 +0800

    Adding @PathVariable

--- spring workspace 2/spring-rest-demo/spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ spring workspace 2/spring-rest-demo/spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -3,7 +3,10 @@ package com.luv2code.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,15 +16,29 @@ import com.luv2code.springdemo.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 	
-	// define endpoint for "/students" - returns list of student
-	@GetMapping("/students")
-	public List<Student> getStudents() {
-		List<Student> students = new ArrayList<>();
+	private List<Student> students;
+	
+	// define @PostConstruct to load the student data .. will get called once
+	@PostConstruct
+	public void loadData() {
+		// better approach because you load data once
+		students = new ArrayList<>();
 
 		students.add(new Student("Joliver", "Estampador"));
 		students.add(new Student("Nathalie", "De Jesus"));
 		students.add(new Student("Badette", "Smith"));
-		
+	}
+	
+	// define endpoint for "/students" - returns list of student
+	@GetMapping("/students")
+	public List<Student> getStudents() {
 		return students;
+	}
+	
+	// define endpoint for /api/student/{studentId}
+	@GetMapping("/student/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		Student student = students.get(studentId);
+		return student;
 	}
 }
--- spring workspace 2/spring-rest-demo/spring-rest-demo/src/main/webapp/index.jsp
+++ spring workspace 2/spring-rest-demo/spring-rest-demo/src/main/webapp/index.jsp
@@ -3,6 +3,7 @@
 		<h2>Spring REST Demo</h2>
 		<hr />
 		
-		<a href="${pageContext.request.contextPath}/test/hello">Hello</a>
+		<a href="${pageContext.request.contextPath}/test/hello">Hello</a> <br>
+		<a href="${pageContext.request.contextPath}/api/students">api/students</a>
 	</body>
 </html>

78ec9b4ddab9432c93f5d3db1d141c7be8fb95cd
Joliver Estampador <joliverestampador@gmail.com>
2020-03-08 12:19:28 +0800

    @COntrollerAdvise for handling multiple rest exceptions

--- spring workspace 2/spring-rest-demo/spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ spring workspace 2/spring-rest-demo/spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -46,26 +46,6 @@ public class StudentRestController {
 		}
 		return students.get(studentId);
 	}
-	
-	// handle errors / exception via StudentNotFoundException
-	@ExceptionHandler
-	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException ex) {
-		StudentErrorResponse error = new StudentErrorResponse();
-		error.setMessage(ex.getMessage());
-		error.setStatus(HttpStatus.NOT_FOUND.value());
-		error.setTimeStamp(System.currentTimeMillis());
-		return new ResponseEntity<StudentErrorResponse>(error, HttpStatus.NOT_FOUND);
-	}
-	
-	// handle generic exception
-	@ExceptionHandler
-	public ResponseEntity<StudentErrorResponse> handleException(Exception ex) {
-		StudentErrorResponse error = new StudentErrorResponse();
-		error.setMessage(ex.getMessage());
-		error.setStatus(HttpStatus.BAD_REQUEST.value());
-		error.setTimeStamp(System.currentTimeMillis());
-		return new ResponseEntity<StudentErrorResponse>(error, HttpStatus.BAD_REQUEST);
-	}
 }
 
 

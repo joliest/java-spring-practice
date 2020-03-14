34ff1a4d1070cd63805be1438ae62756807c3a12
Joliver Estampador <joliverestampador@gmail.com>
2020-03-14 09:50:13 +0800

    Exception handler

--- spring workspace 2/spring-crm-rest-demo/spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestExceptionHandler.java
+++ spring workspace 2/spring-crm-rest-demo/spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestExceptionHandler.java
@@ -20,4 +20,18 @@ public class CustomerRestExceptionHandler {
 		
 		return new ResponseEntity(response, HttpStatus.NOT_FOUND);
 	}
+	
+	// catch all errors
+	@ExceptionHandler
+	public ResponseEntity<CustomerErrorResponse> 
+		handleGenericException(Exception ex) {
+		
+		CustomerErrorResponse response = new CustomerErrorResponse();
+		
+		response.setStatus(HttpStatus.BAD_REQUEST.value());
+		response.setMessage(ex.getMessage());
+		response.setTimeStamp(System.currentTimeMillis());
+		
+		return new ResponseEntity(response, HttpStatus.BAD_REQUEST);
+	}
 }

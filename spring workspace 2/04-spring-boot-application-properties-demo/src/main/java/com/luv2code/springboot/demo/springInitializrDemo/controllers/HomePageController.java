2c7cc7ea988f4101edbaf75d88d3f426c5ce4e62
Joliver Estampador <joliverestampador@gmail.com>
2020-03-22 12:09:39 +0800

    Application.properties demo

--- spring workspace 2/04-spring-boot-application-properties-demo/src/main/java/com/luv2code/springboot/demo/springInitializrDemo/controllers/HomePageController.java
+++ spring workspace 2/04-spring-boot-application-properties-demo/src/main/java/com/luv2code/springboot/demo/springInitializrDemo/controllers/HomePageController.java
@@ -2,11 +2,26 @@ package com.luv2code.springboot.demo.springInitializrDemo.controllers;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomePageController {
+	
+	// inject properties for coach.name and team.name
+	
+	@Value("${coach.name}")
+	private String coachName;
+
+	@Value("${team.name}")
+	private String teamName;
+	
+	// teaminfo
+	@GetMapping("/teaminfo")
+	public String teaminfo() {
+		return "coach name: " + coachName + ", team name: " + teamName; 
+	}
 	
 	// expose
 	@GetMapping("/")

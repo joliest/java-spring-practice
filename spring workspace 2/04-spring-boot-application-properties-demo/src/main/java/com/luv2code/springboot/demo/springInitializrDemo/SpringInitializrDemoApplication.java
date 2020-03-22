ac6625c254b061921bce2f27a5f87e53e8eaaf96
Joliver Estampador <joliverestampador@gmail.com>
2020-03-22 12:05:29 +0800

    Initial Commit for app.properties

---
+++ spring workspace 2/04-spring-boot-application-properties-demo/src/main/java/com/luv2code/springboot/demo/springInitializrDemo/SpringInitializrDemoApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.demo.springInitializrDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInitializrDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInitializrDemoApplication.class, args);
+	}
+
+}

ecf5d16dbfb44b2167395addf01e279a2fd28456
Joliver Estampador <joliverestampador@gmail.com>
2020-03-29 08:59:55 +0800

    Displaying and adding EMployee

--- spring workspace 2/2-thymeleaf-springboot-crud-demo/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ spring workspace 2/2-thymeleaf-springboot-crud-demo/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
 
 @Controller
@@ -18,6 +21,21 @@ public class EmployeeController {
 	@GetMapping("/list")
 	public String showEmployees(Model model) {
 		model.addAttribute("employees", employeeService.findAll());
-		return "employees";
+		return "employees/list-employees";
+	}
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model model) {
+		Employee employee = new Employee();
+		
+		// bind form data
+		model.addAttribute("employee", employee);
+		return "employees/employee-form";
+	}
+	
+	@PostMapping("/save") 
+	public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+		employeeService.save(employee);
+		return "redirect:/employees/list";
 	}
 }
--- spring workspace 2/2-thymeleaf-springboot-crud-demo/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ spring workspace 2/2-thymeleaf-springboot-crud-demo/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -1,8 +1,18 @@
 package com.luv2code.springboot.thymeleafdemo.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+	/*
+	 * Method findAllByOrderByLastNameAsc will be parsed automatically, for more info
+	 * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#repositories.query-methods.details
+	 */
 
+	// custom methods starts here
+	public List<Employee> findAllByOrderByLastNameAsc();
+	
 }
